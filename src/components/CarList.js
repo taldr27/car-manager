99f10dcda78c9b692dab3a5ba110ba0539414519
Diego Garcia <diegogarciaq98@gmail.com>
2023-04-10 12:39:34 -0500

    Add changes to the carlist

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,11 +3,16 @@ import { removeCar } from '../store';
 
 function CarList(){
   const dispatch = useDispatch();
-  const cars = useSelector(({ cars: { data, searchTerm }}) => {
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm }}) => {
 
-    return data.filter((car) => 
+    const filteredCars = data.filter((car) => 
       car.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+
+    return {
+      cars: filteredCars,
+      name: form.name
+    };
   });
 
   const handleCarDelete = (car) => {
@@ -15,8 +20,11 @@ function CarList(){
   };
 
   const renderedCars = cars.map((car) => {
+    //Should be bold?
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
